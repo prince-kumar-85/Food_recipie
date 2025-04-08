d4e44160ed54e5ba0efcc77ba453008db29f6b41
Prince Kumar <princekumarkha2005@gmail.com>
2025-04-08 11:04:16 +0530

    user authentication complete

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -20,9 +20,24 @@ const userSignUp=async (req, res) => {
 }
 
 const userLogin=async (req, res) => {
+    const {email,password} = req.body
+    if(!email || !password){
+        return res.status(400).json({message:"Please provide the valid email ans password"})
+
+    }
+    let user =await User.findOne({email})
+    if(user && await bcrypt.compare(password,user.password)){
+        let token=jwt.sign({email,id:user._id},process.env.SECRET_KEY)
+        return res.status(200).json({token,user})
+    }
+    else{
+        return res.status(400).json({error:"Invalid credentials"})
+    }
 }
 
 const getUser=async (req, res) => {
+    const user =await User.findById(req.params.id)
+    res.json({email:user.email})
 }
 
 
