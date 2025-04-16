f2737be303ad6158e514b0e1d44aca1c130e104b
Prince Kumar <princekumarkha2005@gmail.com>
2025-04-17 00:42:58 +0530

    finish

--- frontend/food-blog-appp/src/components/RecipeItems.jsx
+++ frontend/food-blog-appp/src/components/RecipeItems.jsx
@@ -1,47 +1,66 @@
-import React from 'react'
-import { useLoaderData } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useLoaderData, useNavigate } from 'react-router-dom'
 import foodImg from '../assets/food_recipe_1.jpeg'
-import { FaHeart } from "react-icons/fa";
-import { BsFillStopwatchFill } from "react-icons/bs";
+import { BsStopwatchFill } from "react-icons/bs";
+import { FaHeart } from "react-icons/fa6";
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
-import { Link } from 'react-router-dom'
+import axios from 'axios';
 
-function RecipeItems() {
-    const allRecipes = useLoaderData()
-    let path=window.location.pathname==="/myRecipe" ? true : false
+export default function RecipeItems() {
+    const recipes = useLoaderData()
+    const [allRecipes, setAllRecipes] = useState()
+    let path = window.location.pathname === "/myRecipe" ? true : false
+    let favItems = JSON.parse(localStorage.getItem("fav")) ?? []
+    const [isFavRecipe, setIsFavRecipe] = useState(false)
+    const navigate=useNavigate()
     console.log(allRecipes)
+
+    useEffect(() => {
+        setAllRecipes(recipes)
+    }, [recipes])
+
+    const onDelete = async (id) => {
+        await axios.delete(`http://localhost:5000/recipe/${id}`)
+            .then((res) => console.log(res))
+        setAllRecipes(recipes => recipes.filter(recipe => recipe._id !== id))
+        let filterItem = favItems.filter(recipe => recipe._id !== id)
+        localStorage.setItem("fav", JSON.stringify(filterItem))
+    }
+
+    const favRecipe = (item) => {
+        let filterItem = favItems.filter(recipe => recipe._id !== item._id)
+        favItems = favItems.filter(recipe => recipe._id === item._id).length === 0 ? [...favItems, item] : filterItem
+        localStorage.setItem("fav", JSON.stringify(favItems))
+        setIsFavRecipe(pre => !pre)
+    }
+
     return (
         <>
             <div className='card-container'>
                 {
-                    allRecipes?.map((item,index) => {
+                    allRecipes?.map((item, index) => {
                         return (
-                            <div key={index} className='card'>
-                                <img src={foodImg} width="120" height="100px" ></img>
+                            <div key={index} className='card'onDoubleClick={()=>navigate(`/recipe/${item._id}`)}>
+                                <img src={`http://localhost:5000/images/${item.coverImage}`} width="120px" height="100px"></img>
                                 <div className='card-body'>
                                     <div className='title'>{item.title}</div>
                                     <div className='icons'>
-                                        <div className='timer'>
-                                            <BsFillStopwatchFill />50min
-                                            </div>
-                                          {(!path) ? <FaHeart/> :
+                                        <div className='timer'><BsStopwatchFill />{item.time}</div>
+                                        {(!path) ? <FaHeart onClick={() => favRecipe(item)}
+                                            style={{ color: (favItems.some(res => res._id === item._id)) ? "red" : "" }} /> :
                                             <div className='action'>
-                                           <Link to={`/editRecipe/${item._id}`} className="editIcon"><FaEdit /> </Link>
-                                            <MdDelete className='deleteIcon' />
+                                                <Link to={`/editRecipe/${item._id}`} className="editIcon"><FaEdit /></Link>
+                                                <MdDelete onClick={() => onDelete(item._id)} className='deleteIcon' />
                                             </div>
-                                }
-                                         </div>
+                                        }
+                                    </div>
                                 </div>
                             </div>
-                            
-                        
                         )
-                })
-            }
+                    })
+                }
             </div>
         </>
     )
 }
-
-export default RecipeItems
